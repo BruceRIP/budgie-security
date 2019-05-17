dee8fa64900ed548964af4c17152a9e5c38a8a7f
Bruce29 <rip.bruno.dev@gmail.com>
2019-05-17 12:36:46 -0500

    iniciando readme de proyecto

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # budgie-security
-Security project based on oauth 2.0 protocol
+Budgie Security es un proyecto que permitir maneja y administra la autenticación y autorización de usuarios, roles y permisos para cualquier aplicación. Es un proyecto que utiliza oAuth 2.0 como protocolo de seguridad, lo que le da robustes y permite a muchas aplicaciones realizar una integración rapida y sencilla.
+
+Basado en un protocolo estandar para autorización enfocado en la simplicidad del desarrollo y proporciona flujo de autorización especificos.       
+- Web
+- Mobile
+- Escritorio
+- API´s
+Proporciona funcionalida de autorización que permite a aplicaciones de terceros obtener accesso limitado a recursos HTTP.
+
+![Flujo para solicitud de acceso](https://s3.amazonaws.com/billers-images/oauth_flow.png "Flujo oAuth")
+
--- starting_up.sh
+++ starting_up.sh
@@ -1,9 +1,14 @@
 PATH_BILLERS=/Users/bruno.rivera/Development/GitHub/budgie-security
 echo developerRIP2328 | sudo mongod --bind_ip_all &
-echo developerRIP2328 | sudo activemq start
+#echo developerRIP2328 | sudo activemq start
 sleep 10
 java -jar $PATH_BILLERS/eureka-server/target/eureka-server-1.0-RELEASE-fat.jar &
 java -jar $PATH_BILLERS/cloud-config-server/target/cloud-config-server-1.0-RELEASE.jar &
 sleep 20
 java -jar $PATH_BILLERS/zuul-edge-service/target/zuul-edge-service-1.0-RELEASE-fat.jar &
+sleep 10
 java -jar $PATH_BILLERS/packages-microservice/target/packages-microservice-1.0-RELEASE-fat.jar &
+java -jar $PATH_BILLERS/accounts-microservice/target/accounts-microservice-1.0-RELEASE-fat.jar &
+java -jar $PATH_BILLERS/security-oauth-sso-microservice/target/security-oauth-sso-microservice-1.0-RELEASE-fat.jar &
+java -jar $PATH_BILLERS/register-security-microservice/target/register-security-microservice-0.0.1-SNAPSHOT-fat.jar &
+java -jar $PATH_BILLERS/reporter-microservice/target/reporter-microservice-1.0-ALPHA-fat.jar &
